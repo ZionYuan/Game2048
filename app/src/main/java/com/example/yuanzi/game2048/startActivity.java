b0dddcbbf428d7ea1e1b1a2097736f49b60791e9
yuanzi <yuanzi@gmail.com>
2017-06-11 01:58:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yuanzi/game2048/startActivity.java
@@ -0,0 +1,25 @@
+package com.example.yuanzi.game2048;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class startActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(startActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
